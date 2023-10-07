2d5b59e4918a4674124d33d91479b3e39bfa8e65
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 15:08:35 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,6 +1,12 @@
-import os,platform
+import os
+import platform
+
+# Check for updates from the Git repository
 os.system('git pull')
-# exit(' Wait Tool On updating ')
-Approval=platform.architecture()[0]
-if Approval=="32bit":__import__("usmi32")
-elif Approval=="64bit":__import__("usmi64")
+
+Approval = platform.architecture()[0]
+
+if Approval == "32bit":
+    import usmi32
+elif Approval == "64bit":
+    import usmi64
