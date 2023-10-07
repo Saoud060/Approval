2c505467417599e8e3901c72e6e36777e080b5a9
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:30:44 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,1 +1,9 @@
-import um
+import os
+
+os.system("xdg-open https://www.youtube.com/@awanoficialchannel")
+
+try:
+    import AWN32
+except ImportError as e:
+    if "dlopen failed" in str(e):
+        import um
