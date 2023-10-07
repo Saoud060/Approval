374e60ccc41f7652e2467aad87baff6da265e834
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:36:17 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,13 +1,1 @@
-import subprocess
-
-# Replace 'um.so_url' with the raw URL to the 'um.so' file on GitHub.
-um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
-
-# Download the shared object file.
-subprocess.run(["curl", "-o", "um.so", um_so_url])
-
-# Run the shared object file.
-try:
-    subprocess.run(["./um.so"])
-except Exception as e:
-    print(f"Error: {e}")
+import um
