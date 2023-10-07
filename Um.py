2d9fdcca22e898c4b003905b84c92f5fd6e58362
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:42:39 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,9 +1,24 @@
 import subprocess
+import platform
 
+# Replace 'um.so_url' with the raw URL to the 'um.so' file on GitHub.
 um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
 
+# Determine the architecture of your Android device.
+architecture = platform.architecture()[0]
+
+if "64" in architecture:
+    # 64-bit architecture
+    arch_dir = "x86_64"  # Update this according to the architecture of your um.so file
+else:
+    # 32-bit architecture
+    arch_dir = "x86"  # Update this according to the architecture of your um.so file
+
+# Download the shared object file.
 subprocess.run(["curl", "-o", "um.so", um_so_url])
+
+# Run the shared object file based on the architecture.
 try:
-    subprocess.run(["./um.so"])
+    subprocess.run([f"./{arch_dir}/um.so"])
 except Exception as e:
     print(f"Error: {e}")
