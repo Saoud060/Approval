b6a0c43a9a21cd3381264f93a4ca3f07b5fb924d
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:48:55 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,3 +1,15 @@
 import subprocess
+import platform
+
 um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
-subprocess.run([f"./um.so"])
+
+architecture = platform.machine()
+
+subprocess.run(["curl", "-o", "um.so", um_so_url])
+
+subprocess.run(["chmod", "+x", "um.so"])
+
+try:
+    subprocess.run([f"./um.so"])
+except Exception as e:
+    print(f"Error: {e}")
