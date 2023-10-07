da20dbede7c591bc4a889c3963f0abaf698b238c
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:46:48 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,9 +1,3 @@
 import subprocess
-import platform
-
-# Replace 'um.so_url' with the raw URL to the 'um.so' file on GitHub.
 um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
-
-try:
-    if "64" in architecture:
-        subprocess.run([f"./um.so"])
+subprocess.run([f"./um.so"])
