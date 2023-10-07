c2e566e3667156e95db8dd77e60371a51bbf65bf
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-07 14:45:47 +0500

    Update Um.py

--- Um.py
+++ Um.py
@@ -4,8 +4,6 @@ import platform
 # Replace 'um.so_url' with the raw URL to the 'um.so' file on GitHub.
 um_so_url = "https://github.com/Saoud060/Approval/raw/main/um.so"
 
-
-if "64" in architecture:
-    subprocess.run([f"./um.so"])
-except Exception as e:
-    print(f"Error: {e}")
+try:
+    if "64" in architecture:
+        subprocess.run([f"./um.so"])
