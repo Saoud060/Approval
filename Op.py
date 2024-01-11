c7f3ee64443f7a245681521dc5d60fa115bd391f
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2024-01-11 15:31:06 +0500

    Update Op.py

--- Op.py
+++ Op.py
@@ -93,7 +93,7 @@ def ANZU_UA():
     mdl, bld = random.choice(samsung).split('|')
     ss = "Dalvik/2.1.0 (Linux; U; Android " + ad + "; " + mdl + " Build/" + bld + ") "
     s = "[FBAN/Orca-Android;FBAV/" + str(random.randint(11, 459)) + '.0.0.' + str(random.randrange(9, 99)) + str(random.randint(11, 99)) + ";FBBV/" + str(random.randint(1111111, 9999999))
-    e = ";[FBAN/Orca-Android;FBAV/" + rrr + ";FBPN/com.facebook.orca;FBLC/" + localeh + ";FBBV/"+ str(random.randint(111111111, 999911999)) + ";FBCR/" + fbcr + ";FBMF/samsung;FBBD/samsung;FBDV/" + fff + ";FBSV/" + and_ver + ";FBCA/armeabi-v7a:armeabi;FBDM/" + resolutions + ";FB_FW/1;FBRV/" +  str(random.randint(111111111, 999999999)) + ";]"
+    e = ";[FBAN/Orca-Android;FBAV/" + rrr + ";FBPN/com.facebook.orca;FBLC/" + localeh + ";FBBV/"+ str(random.randint(111111111, 999911999)) + ";FBCR/" + fbcr + ";FBMF/samsung;FBBD/samsung;FBDV/" + fff + ";FBSV/" + and_ver + ";FBCA/armeabi-v7a:armeabi;FBDM/" + resolutions + ";]"
     ua = s + e
 
     return ua
