ee69474f785d53e101b4216d83d1241712ec0ca8
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2024-01-16 10:22:55 +0500

    Update Op.py

--- Op.py
+++ Op.py
@@ -93,8 +93,8 @@ def ANZU_UA():
     mdl, bld = random.choice(samsung).split('|')
     ss = "Dalvik/2.1.0 (Linux;U;Android " + ad + ";" + mdl + " Build/" + bld + ") "
     s = "[FBAN/FB4A;FBAV/" + str(random.randint(11, 99)) + '.0.0.' + str(random.randrange(9, 99)) + str(random.randint(11, 99)) + ";FBBV/" + str(random.randint(1111111, 9999999))
-    e = "[FBAN/FB4A;FBAV/408.1.0.36.103;FBBV/460978449;FBDM/" + resolutions + "; FBLC/" + localeh + ";FBRV/462216735;FBCR/M1;FBMF/Xiaomi;FBBD/Xiaomi;FBPN/com.facebook.katana;FBDV/" + Redm + ";FBSV/" + and_ver + ";FBOP/1;FBCA/arm64-v8a:;]"
-    ek = ";[FBAN/FB4A;FBAV/335.0.0.28.118;FBBV/316528028;FBDM/" + resolutions + ";FBLC/" + localeh + ";FBRV/318280044;FB_FW/2;FBCR/" + fbcr + ";FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/" + fff + ";FBSV/" + and_ver + ";FBOP/19;FBCA/arm64-v8a:;]"
+    e = ";[FBAN/FB4A;FBAV/335.0.0.28.118;FBBV/316528028;FBDM/" + resolutions + ";FBLC//" +  localeh + ";FBRV/318280044;FB_FW/2;FBCR/Hringdu;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/" + fff + ";FBSV/" +  and_ver + ";FBOP/19;FBCA/arm64-v8a:;]"
+    ek = ";[FBAN/FB4A;FBAV/426.0.0.26.50;FBBV/500449097;FBDM/" + resolutions + ";FBLC/" + localeh + ";FBRV/501615570;FB_FW/2;FBCR/ASTC;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/" + fff + ";FBSV/" +  and_ver + ";FBOP/1;FBCA/arm64-v8a:;]"
     ua = s + e
     ua1 = s + ek
     uaa = random.choice([ua, ua1])
@@ -104,7 +104,10 @@ result = ANZU_UA()
 
 ##print(result)
 # Checking if ";FBLC/" is present in the result
-if ";FBLC/" in result:
+if ";FBLC/" in re
+
+
+sult:
     # Extracting locale and country code from the result
     selected_locale = result.split(";FBLC/")[1].split(";")[0]
     # Checking if "_" is present in the selected_locale
